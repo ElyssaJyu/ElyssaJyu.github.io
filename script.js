e7a47bc26f1b163ea24f7d9f9e2f860aeefb2e58
ElyssaJyu <sunelyssa@microsoft.com>
2023-09-13 12:30:12 +0800

    inital commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//history data json structure
+// {
+//     title:[url, visit_count, last_visit_time],
+// }
+//import fetch from 'node-fetch';
+
+fetch(' ', {mode: 'no-cors'})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        //console.log(data);
+        const tableBody = document.querySelector('#historyTable tbody');
+
+        data.forEach(title=>{
+            //console.log(title);
+            let row = document.createElement('tr');
+            let titleCell = document.createElement('td');
+            titleCell.textContent = title;
+            row.appendChild(titleCell);
+
+            let urlCell = document.createElement('td');
+            urlCell.textContent = data[title][0];
+            row.appendChild(urlCell);
+
+            let visitCountCell = document.createElement('td'); 
+            visitCountCell.textContent = data[title][1];
+            row.appendChild(visitCountCell);
+
+            let lastVisitTimeCell = document.createElement('td');
+            lastVisitTimeCell.textContent = data[title][2];
+            row.appendChild(lastVisitTimeCell);
+
+            tableBody.appendChild(row);
+        })
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+
---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = `browser-portrait`;
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME); 
+    cache.addAll([
+      '/', 
+      '/script.js',
+      '/manifest.json',
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+      const cache = await caches.open(CACHE_NAME);
+  
+      if (event.request.url.endsWith('/manifest.json')) {
+        try {
+          const fetchResponse = await fetch(event.request);
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          const cachedResponse = await cache.match(event.request);
+          return cachedResponse || new Response("Error fetching manifest", { status: 404 });
+        }
+      }
+  
+      const cachedResponse = await cache.match(event.request); 
+      if (cachedResponse) {
+        return cachedResponse; 
+      } else {
+        try {
+          const fetchResponse = await fetch(event.request); 
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // Handle fetch error
+        }
+      }
+    })());
+});
+  
