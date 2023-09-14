47157d436c3b23b647fefbc7642b80aa055ae1e3
ElyssaJyu <sunelyssa@microsoft.com>
2023-09-14 13:26:31 +0800

    pie chart

--- script.js
+++ script.js
@@ -3,91 +3,110 @@
 //     title:[url, visit_count, last_visit_time],
 // }
 
-fetch('mockdata.json')
-    .then(response => {
+async function getLast7DaysData() {
+    try {
+        const response = await fetch("/mockdata.json");
         if (!response.ok) {
-            throw new Error('Network response was not ok.');
+            throw new Error('Failed to fetch data from the server.');
         }
-        return response.json();
-    })
-    .catch(error => {
-        console.error('Error:', error.message);
-    })
-    .then(data => {
-        //console.log(data);
+        const data = await response.json();
+        console.log(data);
 
-        //Top 3 clicked websites and their avg stay duration.
-        const sortedWebsites = Object.entries(data).sort((a, b) => b[1][1] - a[1][1]);
-        const topThreeWebsites = sortedWebsites.slice(0, 3).map(item => item[0]).join(', ');
-        document.getElementById('websites').textContent = topThreeWebsites;
-        //TODO: calculate avg stay duration
-        document.getElementById('duration').textContent = '5 minutes';
+        // Get the date from 7 days ago
+        const sevenDaysAgo = new Date();
+        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
 
-        const tableBody = document.querySelector('#historyTable tbody');
-
-        data.forEach(title => {
-            //console.log(title);
-            let row = document.createElement('tr');
-            let titleCell = document.createElement('td');
-            titleCell.textContent = title;
-            row.appendChild(titleCell);
-
-            let urlCell = document.createElement('td');
-            urlCell.textContent = data[title][0];
-            row.appendChild(urlCell);
-
-            let visitCountCell = document.createElement('td');
-            visitCountCell.textContent = data[title][1];
-            row.appendChild(visitCountCell);
-
-            let lastVisitTimeCell = document.createElement('td');
-            lastVisitTimeCell.textContent = data[title][2];
-            row.appendChild(lastVisitTimeCell);
-
-            tableBody.appendChild(row);
-        })
-
-        // Pie chart
-        let labels = Object.keys(data);
-        let visitCounts = labels.map(label => data[label][1]);
-
-        const dataLength = data.length;
-        const backgroundColors = [];
-
-        for (let i = 0; i < dataLength; i++) {
-            backgroundColors.push(getRandomColor());
-        }
-
-        const ctx = document.getElementById('PieChart').getContext('2d');
-        const chart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: labels,
-                datasets: [{
-                    data: visitCounts,
-                    backgroundColor: backgroundColors
-                }]
-            },
-            options: {
-                responsive: true,
-                legend: {
-                    position: 'top',
-                },
-                title: {
-                    display: true,
-                    text: 'Website Visit Counts'
-                },
-                animation: {
-                    animateScale: true,
-                    animateRotate: true
-                }
-            }
+        // Filter the data to get only the records from the last 7 days
+        const last7DaysData = Object.entries(data).filter(([title, [url, visit_count, last_visit_time]]) => {
+            const visitDate = new Date(last_visit_time);
+            return visitDate >= sevenDaysAgo;
         });
+        console.log(last7DaysData);
 
+        return last7DaysData;
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function createTable(data) {
+    const tableBody = document.querySelector('#historyTable tbody');
+
+    data.forEach(([title, details]) => {
+        let row = document.createElement('tr');
+        let titleCell = document.createElement('td');
+        titleCell.textContent = title;
+        row.appendChild(titleCell);
+
+        let urlCell = document.createElement('td');
+        urlCell.textContent = details[0];
+        row.appendChild(urlCell);
+
+        let visitCountCell = document.createElement('td');
+        visitCountCell.textContent = details[1];
+        row.appendChild(visitCountCell);
+
+        let lastVisitTimeCell = document.createElement('td');
+        lastVisitTimeCell.textContent = details[2];
+        row.appendChild(lastVisitTimeCell);
+
+        tableBody.appendChild(row);
     })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+}
+
+//Top 3 clicked websites and their total stay duration.
+function GetTopThreeWebsites(data) {
+
+    const sortedWebsites = Object.entries(data).sort((a, b) => b[1][1] - a[1][1]);
+    console.log(sortedWebsites);
+    const topThreeWebsites = sortedWebsites.slice(0, 3).map(item => item[0]).join(', ');
+    console.log(topThreeWebsites);
+    const topThreeWebsitesDuration = sortedWebsites.slice(0, 3).map(item => item[1][2]).join(', ');
+    console.log(topThreeWebsitesDuration);
+    document.getElementById('websites').textContent = topThreeWebsites;
+    //TODO: calculate total stay duration over the last 7 days, currently it is visits count.
+    document.getElementById('duration').textContent = topThreeWebsitesDuration;
+}
+
+function DrawPieChart(data) {
+    // Pie chart
+    let LabelsArray = Object.keys(data);
+    let visitCounts = labels.map(label => data[label][1]);
+
+    const dataLength = data.length;
+    const backgroundColors = [];
+
+    for (let i = 0; i < dataLength; i++) {
+        backgroundColors.push(getRandomColor());
+    }
+
+    const ctx = document.getElementById('PieChart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: LabelsArray,
+            datasets: [{
+                data: visitCounts,
+                backgroundColor: backgroundColors
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Website Visit Counts'
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
     });
+}
 
 //randowm color generator for PieChart
 function getRandomColor() {
@@ -99,3 +118,11 @@ function getRandomColor() {
     return color;
 }
 
+async function main() {
+    const data = await getLast7DaysData();
+    createTable(data);
+    GetTopThreeWebsites(data);
+    DrawPieChart(data);
+}
+
+main();
