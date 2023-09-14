62f6192e40436f0f03d675dcbc759b8ef647b93f
ElyssaJyu <sunelyssa@microsoft.com>
2023-09-14 17:07:18 +0800

    bar chart

--- script.js
+++ script.js
@@ -26,12 +26,23 @@ async function getLast7DaysData(data) {
 
         createTable(last7DaysData);
         GetTopThreeWebsites(last7DaysData);
-        DrawPieChart(last7DaysData);
+        DrawBarChart(last7DaysData);
 
     } catch (error) {
         console.error('Error:', error);
     }
 
+}
+
+function extractMainDomain(urlString) {
+    const match = urlString.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+        const parts = match[2].split('.');
+        if (parts.length > 1) {
+            return parts[parts.length - 2];
+        }
+    }
+    return null;
 }
 
 function createTable(data) {
@@ -40,7 +51,8 @@ function createTable(data) {
     data.forEach(([title, details]) => {
         let row = document.createElement('tr');
         let titleCell = document.createElement('td');
-        titleCell.textContent = details[0];
+        const domainName = extractMainDomain(details[1]);
+        titleCell.textContent = domainName;
         row.appendChild(titleCell);
 
         let urlCell = document.createElement('td');
@@ -65,7 +77,7 @@ function GetTopThreeWebsites(data) {
     const sortedData = Object.entries(data).sort((a, b) => b[1][2] - a[1][2]);
     const topThree = sortedData.slice(0, 3).map(([id, entry]) => {
         return {
-            name: entry[1],
+            name: extractMainDomain(entry[1]),
             clickCount: entry[3]
         };
     });
@@ -75,29 +87,24 @@ function GetTopThreeWebsites(data) {
     const visitCounts = topThree.map(entry => entry.clickCount).join(', ');
 
     document.getElementById('websites').textContent = websiteNames;
-    document.getElementById('visits').textContent = visitCounts;
+    //document.getElementById('visits').textContent = visitCounts;
 
 }
 
-function DrawPieChart(data) {
-    // Pie chart
-    let LabelsArray = Object.values(data).map(item => item[1][0]);
-    console.log(LabelsArray);
+function DrawBarChart(data) {
+    // Bar chart
+    let labelsArray = Object.values(data).map(item => extractMainDomain(item[1][1]));
     let visitCounts = Object.values(data).map(item => parseInt(item[1][2]));
 
-    const dataLength = data.length;
-    const backgroundColors = [];
+    const backgroundColors = labelsArray.map(() => getRandomColor());
 
-    for (let i = 0; i < dataLength; i++) {
-        backgroundColors.push(getRandomColor());
-    }
-
-    const ctx = document.getElementById('PieChart').getContext('2d');
+    const ctx = document.getElementById('BarChart').getContext('2d');
     const chart = new Chart(ctx, {
-        type: 'pie',
+        type: 'bar',
         data: {
-            labels: LabelsArray,
+            labels: labelsArray,
             datasets: [{
+                label: 'Visit Counts',
                 data: visitCounts,
                 backgroundColor: backgroundColors
             }]
@@ -111,13 +118,17 @@ function DrawPieChart(data) {
                 display: true,
                 text: 'Website Visit Counts'
             },
-            animation: {
-                animateScale: true,
-                animateRotate: true
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
             }
         }
     });
 }
+
 
 //randowm color generator for PieChart
 function getRandomColor() {
